24138f9c0b5abc72dce5d12f297f2b91a438ee81
Kofeinich <kofeinich1@gmail.com>
2023-05-09 04:19:26 +0300

    feat: ver 1.0.0

--- internal/services/botService/updateBotConfig.go
+++ internal/services/botService/updateBotConfig.go
@@ -11,13 +11,12 @@ func (s Service) UpdateBotConfig(conf states.BotStates, id string) error {
 	if err != nil {
 		return err
 	}
+	botToken, err := s.r.GetBotTokenByID(id)
 
 	err = s.r.CreateBotConfig(id, marshalledJSON)
 	if err != nil {
 		return err
 	}
-
-	botToken, err := s.r.GetBotTokenByID(id)
 
 	err = tgService.RegNewWebHook(botToken, id)
 	if err != nil {
